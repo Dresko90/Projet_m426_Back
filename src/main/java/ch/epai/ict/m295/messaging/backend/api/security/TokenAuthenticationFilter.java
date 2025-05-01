aafa82230c429d820d001e218a9674928629b21f
Jérôme Frossard <frossardj@edufr.ch>
2025-05-01 21:32:32 +0200

    update

--- src/main/java/ch/epai/ict/m295/messaging/backend/api/security/TokenAuthenticationFilter.java
+++ src/main/java/ch/epai/ict/m295/messaging/backend/api/security/TokenAuthenticationFilter.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,7 +12,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.server.ResponseStatusException;
 
 import ch.epai.ict.m295.messaging.backend.domain.User;
 import ch.epai.ict.m295.messaging.backend.domain.security.Token;
